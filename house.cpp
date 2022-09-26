3faaadeccf35000024c1e7295472fa7545bfe47a
sam-levin <sam.ky.levin@gmail.com>
2022-09-25 22:53:30 -0700

    adding more math functions

--- house.cpp
+++ house.cpp
@@ -4,25 +4,30 @@ using namespace std;
 int main () {
 
     // instantiating all variables 
-    double width, wallHeight, totalHeight, roofHeight, windowWidth, windowHeight, totalSf, cansOfPaint; 
+    double width, wallHeight, totalHeight, roofHeight, windowWidth, windowHeight, roofSf, wallSf, totalSf, cansOfPaint, windowArea, sfPerCan; 
 
+    sfPerCan = 400;
     std::cout << "Welcome to paint calculator 2022! \n \n";
 
     std::cout << "How wide is the wall in feet? ";
     std::cin >> width;
-    
     std::cout << "and how high is the wall to the bottom of the roof? ";
     std::cin >> wallHeight;
-    
+    wallSf = width * wallHeight;
     std::cout << "and how high is the wall to the top of the roof? ";
     std::cin >> totalHeight;
-    
+    roofHeight = totalHeight - wallHeight; 
+    roofSf = .5 * roofHeight * width;
+
     std::cout << "How wide is the window (in feet)? ";
     std::cin >> windowWidth;
-    
     std::cout << "and what is the windows width? ";
     std::cin >> windowHeight;
-    
+    windowArea = windowHeight * windowWidth;
+    totalSf = (roofSf + wallSf) - windowArea;
+
+    cansOfPaint = totalSf / sfPerCan;
+
     std::cout << "\nA side wall that is";
     std::cout << "' wide and ";
     std::cout << "' wide and ";
