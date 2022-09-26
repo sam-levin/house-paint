cf668b8357d85001e673608d04cf268292c7c07e
sam-levin <sam.ky.levin@gmail.com>
2022-09-25 20:11:56 -0700

    adding more code

---
+++ house.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    double width, wallHeight, totalHeight, roofHeight, windowWidth, windowHeight, totalSf, cansOfPaint; 
+
+    std::cout << "Welcome to paint calculator 2022! \n \n";
+
+    std::cout << "How wide is the wall in feet?";
+    std::cin >> width;
+    
+    std::cout << "\nand how high is the wall to the bottom of the roof?";
+    std::cin >> wallHeight;
+    
+    std::cout << "\nand how high is the wall to the top of the roof?";
+    std::cin >> totalHeight;
+    
+    std::cout << "\nHow wide is the window (in feet)?";
+    std::cin >> windowWidth;
+    
+    std::cout << "\nand what is the windows width?";
+    std::cin >> windowHeight;
+    
+}
